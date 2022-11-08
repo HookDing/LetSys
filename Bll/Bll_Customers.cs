05eabc9eccb70c3fdd7f71a602d13cdf4ca552f4
HookDing <HookDing02@outlook.com>
2022-11-08 11:12:55 +0800

    Add Bll/BLL_Customer Add /LetSys/Controllers/CustomersController Init LetSys/Views/Customer/Index.cshml

---
+++ Bll/Bll_Customers.cs
@@ -0,0 +1,89 @@
+﻿using Dal;
+using Mod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bll
+{
+    public class Bll_Customers
+    {
+        public static List<Mod_Customers> GetAllMod()
+        {
+            using (LetDB db = new LetDB())
+            {
+                return db.Customers
+                    .Where(t => t.CusState > 0)
+                    .Select(n => new Mod_Customers
+                    {
+                        CusID = n.CusID,
+                        CusName = n.CusName,
+                        CusSex = n.CusSex,
+                        CusCard = n.CusCard,
+                        CusState = n.CusState,
+                        CusTel = n.CusTel
+                    }).ToList();
+            }
+        }
+        public static Mod_Customers GetMod(int id)
+        {
+            using (LetDB db = new LetDB())
+            {
+                return db.Customers
+                    .Where(n => n.CusID == id && n.CusState > 0)
+                    .Select(n => new Mod_Customers
+                    {
+                        CusID = n.CusID,
+                        CusName = n.CusName,
+                        CusSex = n.CusSex,
+                        CusCard = n.CusCard,
+                        CusTel = n.CusTel,
+                        CusState = n.CusState,
+                    }).ToList()[0];
+            }
+        }
+        public static bool Insert(string CusName, string CusSex, string CusCard, string CusTel)
+        {
+            using (LetDB db = new LetDB())
+            {
+                Customers info = new Customers
+                {
+                    CusName = CusName,
+                    CusSex = CusSex,
+                    CusCard = CusCard,
+                    CusTel = CusTel
+                };
+                db.Customers.Add(info);
+                return db.SaveChanges() > 0;
+            }
+        }
+        public static bool Update(int CusID, string CusName, string CusSex, string CusCard, string CusTel)
+        {
+            using (LetDB db = new LetDB())
+            {
+                Customers info = new Customers
+                {
+                    CusID = CusID,
+                    CusName = CusName,
+                    CusSex = CusSex,
+                    CusCard = CusCard,
+                    CusTel = CusTel
+                };
+                db.Entry(info).State = System.Data.Entity.EntityState.Modified;
+                return db.SaveChanges() > 0;
+            }
+        }
+        public static bool Delete(int CusID)
+        {
+            using (LetDB db = new LetDB())
+            {
+                Customers info = db.Customers.Find(CusID);
+                info.CusState = 0;
+                db.Entry(info).State = System.Data.Entity.EntityState.Modified;
+                return db.SaveChanges() > 0;
+            }
+        }
+    }
+}
---
+++ LetSys/Controllers/CustomersController.cs
@@ -0,0 +1,38 @@
+﻿using Bll;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LetSys.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetAllMod()
+        {
+            return Json(Bll_Customers.GetAllMod());
+        }
+        public ActionResult GetMod(int id)
+        {
+            return Json(Bll_Customers.GetMod(id));
+        }
+        public ActionResult Insert(string CusName, string CusSex, string CusCard, string CusTel)
+        {
+            return Json(Bll_Customers.Insert(CusName,CusSex,CusCard,CusTel));
+        }
+        public ActionResult Update(int CusID, string CusName, string CusSex, string CusCard, string CusTel)
+        {
+            return Json(Bll_Customers.Update(CusID,CusName,CusSex,CusCard,CusTel));
+        }
+        public ActionResult Delete(int CusID)
+        {
+            return Json(Bll_Customers.Delete(CusID));
+        }
+    }
+}
