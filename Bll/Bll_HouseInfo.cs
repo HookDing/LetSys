6538afd6787486008421dc9cb55c0f0adb5134c0
HookDing <HookDing02@outlook.com>
2022-11-10 12:13:55 +0800

    add HouseInfo

---
+++ Bll/Bll_HouseInfo.cs
@@ -0,0 +1,84 @@
+﻿using Dal;
+using Mod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bll
+{
+    public class Bll_HouseInfo
+    {
+        /// <summary>
+        /// 获取所有记录(可根据类型HCID查询)
+        /// </summary>
+        /// <returns></returns>
+        public static List<Mod_HouseInfo> GetPageMod(int page, int size, Mod_HouseInfo info, out int count)
+        {
+            using (LetDB db = new LetDB())
+            {
+                var where = db.HouseInfo.Where(n => n.HState > 0);
+                if (info.HCID > -1)
+                {
+                    where = where.Where(n => n.HCID == info.HCID);
+                }
+                var list = where.OrderByDescending(n => n.HID).Skip((page - 1) * size).Take(size)
+                  .Select(n => new Mod_HouseInfo
+                  {
+                      HID = n.HID,
+                      HAdd = n.HAdd,
+                      HArea = n.HArea,
+                      HDirection = n.HDirection,
+                      HMark = n.HMark,
+                      HNet = n.HNet,
+                      HNum = n.HNum,
+                      HRent = n.HRent,
+                      HType = n.HType,
+                      HElectric = n.HElectric,
+                      HElectricMoney = n.HElectricMoney,
+                      HWater = n.HWater,
+                      HWaterMoney = n.HWaterMoney,
+                      HState = n.HState,
+                      HCID = n.HCID,
+                      HCName = n.HouseCategory.HCName,
+                  }).ToList();
+                count = where.Count();
+                return list;
+            }
+        }
+        /// <summary>
+        /// 根据ID获取一条记录
+        /// </summary>
+        /// <param name="HCID">房屋类型ID</param>
+        /// <returns></returns>
+        public static Mod_HouseInfo GetMod(int HID)
+        {
+            using (LetDB db = new LetDB())
+            {
+                return db.HouseInfo
+                    .Where(n => n.HState > 0 && n.HID == HID)
+                    .Select(n => new Mod_HouseInfo
+                    {
+                        HID = n.HID,
+                        HAdd = n.HAdd,
+                        HArea = n.HArea,
+                        HDirection = n.HDirection,
+                        HMark = n.HMark,
+                        HNet = n.HNet,
+                        HNum = n.HNum,
+                        HRent = n.HRent,
+                        HType = n.HType,
+                        HElectric = n.HElectric,
+                        HElectricMoney = n.HElectricMoney,
+                        HWater = n.HWater,
+                        HWaterMoney = n.HWaterMoney,
+                        HState = n.HState,
+                        HCID = n.HCID,
+                        HCName = n.HouseCategory.HCName,
+                    }).ToList()[0];
+
+            }
+        }
+    }
+}
---
+++ LetSys/Controllers/HouseInfoController.cs
@@ -0,0 +1,44 @@
+﻿using Bll;
+using Mod;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LetSys.Controllers
+{
+    public class HouseInfoController : Controller
+    {
+        // GET: HouseInfo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetTree()
+        {
+            var list = Bll_HouseCategory.GetAllMod().Select(n => new
+            {
+                id = n.HCID,
+                text = n.HCName,
+            }).ToList();
+            //创建树状列表
+            var arr = new ArrayList();
+            //向列表中添加数据(new{值,文本,子集})
+            arr.Add(new { id = -1, text = "全部", children = list });
+            return Json(arr);
+        }
+        public ActionResult GetPageMod(int page, int rows, Mod_HouseInfo info)
+        {
+            int count;
+            if (info == null)
+            {
+                info = new Mod_HouseInfo();
+            }
+            var list = Bll_HouseInfo.GetPageMod(page, rows, info, out count);
+            return Json(new { rows = list, total = count });
+
+        }
+    }
+}
