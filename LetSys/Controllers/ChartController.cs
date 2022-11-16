dc9a63c0f5be5f4c2af9f7612415bb949b8fe290
HookDing <HookDing@DESKTOP-IITRI4K>
2022-11-16 10:24:32 +0800

    Add Chart1(HouseCategory eCharts)

---
+++ LetSys/Controllers/ChartController.cs
@@ -0,0 +1,24 @@
+﻿using Bll;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LetSys.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetData1(string value)
+        {
+            var date = DateTime.Parse(value);
+            var list =Bll_PayInfo.GetChart1(date);
+            return Json(list);
+        }
+    }
+}
