c00407758077285a2584e29700b2c5b4b675e83e
HookDing <HookDing@DESKTOP-IITRI4K>
2022-11-21 23:10:47 +0800

    Add ChargeCategory

---
+++ LetSys/Controllers/ChargeCategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LetSys.Controllers
+{
+    public class ChargeCategoryController : Controller
+    {
+        // GET: ChargeCategory
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
